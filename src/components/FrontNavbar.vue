cd5754f64b209f8f6ef0173bcfe9d266c196569b
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-09-02 20:43:31 +0800

    project init

---
+++ src/components/FrontNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bg-main bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 d-flex justify-content-between">
+                    <div class="logo"></div>
+                    <ul class="d-flex">
+                        <li>
+                            <a href="#">課程介紹</a>
+                        </li>
+                        <li>
+                            <a href="#">瑜珈空間</a>
+                        </li>
+                        <li>
+                            <a href="#">當月課表</a>
+                        </li>
+                    </ul>
+                    <ul class="d-flex">
+                        <li>
+                            <button></button>
+                        </li>
+                        <li>
+                            <button></button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
